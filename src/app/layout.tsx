c0ae442417b4bf6d61c50e9060f1d121bcb0879b
Franziac <fransjarvi@gmail.com>
2024-08-01 17:10:32 +0300

    favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,15 @@ export default function RootLayout({
     <SessionWrapper>
     <html lang="en" className='font-main'>
       <head>
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#8b5cf6"/>
+        <meta name="msapplication-TileColor" content="#8b5cf6"/>
+        <meta name="theme-color" content="#8b5cf6"/>
+
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900" rel="stylesheet"/>
       </head>
